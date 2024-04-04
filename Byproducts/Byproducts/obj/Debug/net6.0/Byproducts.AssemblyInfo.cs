a053583e9d0ceaa1a3925913e76df426171f711d
Manon Lefebvre <manonalefebvre@gmail.com>
2024-04-04 00:58:31 -0700

    finished adding buckwheat waffles

--- Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
+++ Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5703dc6803cd5df58b626ceae9dbc0f2936adc13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70adcba4ab7c5441a90eee44a4d887714066c3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
