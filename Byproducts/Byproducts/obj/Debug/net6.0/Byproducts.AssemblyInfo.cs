c34677c8323f5c643cf1bba0c12a69573bcaa0d6
Manon Lefebvre <manonalefebvre@gmail.com>
2024-04-11 23:17:39 -0700

    formatting

--- Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
+++ Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d04bb5d517e38232983c312d721ab432ce25415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e87d79995c19c5eb262bc523d1b0757b3ad569")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
