6e37bd80e1db1af72ac633e1f27764fd0825e514
Manon Lefebvre <manonalefebvre@gmail.com>
2024-04-14 15:10:04 -0700

    added config translations and QL Flower Garden 2 compatibility option.

--- Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
+++ Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72f449584f7ef46f5711b8c51abffe9359b84fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f6c647c8afbf7e25fd69d95464fdc59860eaa94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
