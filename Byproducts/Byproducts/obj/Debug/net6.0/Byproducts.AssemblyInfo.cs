8d4ddc5cf1be769a0eff6750c6631a381e64b13c
Manon Lefebvre <manonalefebvre@gmail.com>
2024-04-14 17:30:48 -0700

    added compatibility for QLFG2 and fixed regrow spritesheet for sugarcane

--- Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
+++ Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f6c647c8afbf7e25fd69d95464fdc59860eaa94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e37bd80e1db1af72ac633e1f27764fd0825e514")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
