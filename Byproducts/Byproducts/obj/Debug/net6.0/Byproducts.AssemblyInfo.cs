b7b01e94873860c7e522dfc769b286defa936243
Manon Lefebvre <manonalefebvre@gmail.com>
2024-04-16 07:16:56 -0700

    fixed tea recipes always showing as not gotten & fixed moon cake recipe showing as no symbol and added regular tea leaves to make green tea

--- Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
+++ Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8459fc8c0030bb27c13c558f834a5f1693ccc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee072a5e904d4cea24fddb0d26dfefe686b6938a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
