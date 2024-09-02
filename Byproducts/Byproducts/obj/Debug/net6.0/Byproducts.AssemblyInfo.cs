b2beefbf9c8285c75ef3170e4f5619936e271c0d
Manon Lefebvre <manonalefebvre@gmail.com>
2024-09-02 14:07:22 -0700

    fixed double seeds in Qi shop and fixed seasons for sorghum seeds

--- Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
+++ Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf3402c13a8a27f4cfda4562c548c290f0dc1cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4ef7b47eed7cfc2062599eddb7d68b17c15688")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
