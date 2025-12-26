12993f176ba540d482929fff6975c61c3eef01a4
Manon Lefebvre <manonalefebvre@gmail.com>
2025-12-26 08:09:56 -0800

    actually put cornucopia compatibility and also applied config rules better

--- Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
+++ Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4ef7b47eed7cfc2062599eddb7d68b17c15688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d88740ba80e959221c67b74ffbb6c94694b53e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
