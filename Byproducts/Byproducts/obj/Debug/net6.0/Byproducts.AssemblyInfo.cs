959ae3cbfccacca198bcb2a782044f6baa8a871a
Manon Lefebvre <manonalefebvre@gmail.com>
2025-12-26 09:39:37 -0800

    fix typo and grains preserves jar

--- Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
+++ Byproducts/Byproducts/obj/Debug/net6.0/Byproducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d31502dc069b16217728e50f15e4683f2b2a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+768a51f518f6727f8c805d1242486a9d7919807a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byproducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
